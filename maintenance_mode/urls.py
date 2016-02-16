ebfbd548dc0643a37537235469e26db86a9bbac8
Pieter Wellens <pieter.wellens@gmail.com>
2016-02-16 13:44:01 +0100

    fix for Django 1.9
    Django 1.9 gives a warning about string based views referenced in urls.py

--- maintenance_mode/urls.py
+++ maintenance_mode/urls.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, url
+from maintenance_mode.views import maintenance_mode_off, maintenance_mode_on
 
 
 urlpatterns = patterns('',
-    url(r'^off/$', 'maintenance_mode.views.maintenance_mode_off', name='maintenance_mode_off'), 
-    url(r'^on/$', 'maintenance_mode.views.maintenance_mode_on', name='maintenance_mode_on'), 
+    url(r'^off/$', maintenance_mode_off, name='maintenance_mode_off'), 
+    url(r'^on/$', maintenance_mode_on, name='maintenance_mode_on'), 
 )
 
